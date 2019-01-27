19d3bba2fdc86116fc810dc9c7d95e40296c5263
Stef Claus <stef.claus@gmail.com>
2019-01-27 19:46:00 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -37,7 +37,7 @@ class Application
       @@cart << add_item
       return "added #{add_item}"
     else
-      return "Couldn't add #{add_item} because we are out."
+      return "We don't have that item"
     end #ends if
   end #ends method
 
